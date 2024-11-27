e4bfcbbd034ea7f061f3fa6fb9b004e45cc54ee0
BartekK82 <bajtekkajtek2008@gmail.com>
2024-11-27 09:35:52 +0100

    Coiny

--- Projekt/Assets/Scripts/PickUp.cs
+++ Projekt/Assets/Scripts/PickUp.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class PickUp : MonoBehaviour
 {
-    int score = 0;
+    float score = 0;
+    [SerializeField] float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +21,44 @@ public class PickUp : MonoBehaviour
     {
         if (other.gameObject.tag == ("Coin"))
         {
-            Destroy(other.gameObject);
+            
             score++;
             Debug.Log("Twój wynik to " + score);
+            StartCoroutine(MoveToPlayer(other.transform, transform, speed));
+         }
+
+         IEnumerator MoveToPlayer(Transform coin, Transform player, float speed)
+        {
+            float duration = 3.0f;
+            float time = 0;
+
+            while (time < duration)
+            {
+                coin.position = Vector3.MoveTowards(coin.position, player.position, speed*Time.deltaTime);
+                time += Time.deltaTime; // nasz licznik czasu - 0,05 + 0,05 + 0,05 = 3 
+
+                yield return null; // odstêp czasowy - 1 stop klatka
+
+            }
+
+         
         }
-
-
-
-
-
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject); ;
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+
 }
